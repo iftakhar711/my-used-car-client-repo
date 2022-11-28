4e96e5d833a3da70eb782e7a25bb51e04f495f45
iftakhar711 <iftakharudiin80@gmail.com>
2022-11-28 20:44:17 +0600

    book

--- src/Components/Home/ProductsCollection/BookingModal.js
+++ src/Components/Home/ProductsCollection/BookingModal.js
@@ -1,53 +1,51 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../Contexts/AuthProvider';
-// import toast from 'react-hot-toast';
+import toast from 'react-hot-toast';
 
 const BookingModal = ({ proinfo }) => {
     const { user } = useContext(AuthContext);
 
-    // const handleBooking = event => {
-    //     event.preventDefault();
-    //     const form = event.target;
-    //     const slot = form.slot.value;
-    //     const name = form.name.value;
-    //     const email = form.email.value;
-    //     const phone = form.phone.value;
-    //     // [3, 4, 5].map((value, i) => console.log(value))
-    //     const booking = {
-    //         appointmentDate: date,
-    //         treatment: treatmentName,
-    //         patient: name,
-    //         slot,
-    //         email,
-    //         phone,
-    //         price
-    //     }
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+        const productname = form.productname.value;
+        const name = form.name.value;
+        const email = form.email.value;
+        const resaleprice = form.resaleprice.value;
+        const location = form.location.value;
+        const phone = form.phone.value;
 
-    //     // TODO: send data to the server
-    //     // and once data is saved then close the modal 
-    //     // and display success toast
-    //     fetch('http://localhost:5000/bookings', {
-    //         method: 'POST',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(booking)
-    //     })
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data);
-    //             if (data.acknowledged) {
+        const booking = {
+            productname,
+            name,
+            email,
+            resaleprice,
+            location,
+            phone
+        }
 
-    //                 toast.success('Booking confirmed');
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.acknowledged) {
 
-    //             }
-    //             else{
-    //                 toast.error(data.message);
-    //             }
-    //         })
+                    toast.success('Booking confirmed');
+
+                }
+                else {
+                    toast.error(data.message);
+                }
+            })
 
 
-    // }
+    }
 
     return (
         <>
@@ -56,10 +54,11 @@ const BookingModal = ({ proinfo }) => {
                 <div className="modal-box relative">
                     <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{proinfo?.productname}</h3>
-                    <form className='grid grid-cols-1 gap-3 mt-10'>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
+                        <input name="productname" type="text" defaultValue={proinfo?.productname} disabled placeholder="Your Name" className="input w-full input-bordered" />
                         <input name="name" type="text" defaultValue={user?.displayName} disabled placeholder="Your Name" className="input w-full input-bordered" />
-                        <input name="email" type="email" defaultValue={user?.email} disabled placeholder="Email Address" className="input w-full input-bordered" />
-                        <input name="resaleprice" type="text" defaultValue={`${proinfo?.resaleprice} Taka`} disabled placeholder="Email Address" className="input w-full input-bordered" />
+                        <input name="email" type="email" defaultValue={user?.email} disabled className="input w-full input-bordered" />
+                        <input name="resaleprice" type="text" defaultValue={proinfo?.resaleprice} disabled className="input w-full input-bordered" />
                         <input type='text' name='location' id='location' placeholder='Enter location' className="input w-full input-bordered" />
                         <input name="phone" type="text" placeholder="Phone Number" className="input w-full input-bordered" />
                         <br />
