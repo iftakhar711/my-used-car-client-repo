f347a890467204884f2f6d0223cd324294de688e
iftakhar711 <iftakharudiin80@gmail.com>
2022-11-28 23:23:12 +0600

    ad

--- src/Components/Dashboard/Myproduct.js
+++ src/Components/Dashboard/Myproduct.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 const Myproduct = () => {
+    const { data: myproduct = [] } = useQuery({
+        queryKey: ['myproduct'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/bookings');
+            const data = await res.json();
+            return data;
+        }
+    });
     return (
-        <div>
-            hq
+        <div className=' grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            {
+                myproduct?.map(product =>
+                    <div className="card w-96 bg-base-100 shadow-xl image-full">
+                        <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{product?.productname}</h2>
+                            <p>{product?.name}</p>
+                            <p>{product?.email}</p>
+                            <p>Price: {product?.resaleprice}</p>
+                            <p>location:{product?.location}</p>
+                            <p>Contact:{product?.phone}</p>
+
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
